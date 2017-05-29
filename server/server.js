3b43e25cfb1c3177dcb88ca4736e15c47a5ff66d
PeterCondick <pcondick@Peters-MacBook-Pro.local>
2017-05-29 21:30:37 +1000

    greet new user

--- server/server.js
+++ server/server.js
@@ -15,11 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new connection');
 
-  // socket.emit('newMessage', {
-  //   from: 'Mr Hairy',
-  //   text: 'im getting hairier',
-  //   createdAt: 123
-  // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user just joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
@@ -28,6 +34,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //    from: message.from,
+    //    text: message.text,
+    //    createdAt: new Date().getTime()
+    //  });
   });
 
   socket.on('disconnect', () => {
