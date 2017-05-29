2844dcec575bde754da7cd3cf7fd2c4bfcddc81e
PeterCondick <pcondick@Peters-MacBook-Pro.local>
2017-05-29 21:08:26 +1000

    emit message

--- server/server.js
+++ server/server.js
@@ -15,14 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new connection');
 
-  socket.emit('newMessage', {
-    from: 'Mr Hairy',
-    text: 'im getting hairier',
-    createdAt: 123
-  });
+  // socket.emit('newMessage', {
+  //   from: 'Mr Hairy',
+  //   text: 'im getting hairier',
+  //   createdAt: 123
+  // });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
